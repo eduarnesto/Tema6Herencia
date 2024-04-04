a684feb6f90b6ce23a6a6b4ca9cf14353f3ba1a3
eduarnesto <eduardo.arnesto@iesnervion.es>
2024-04-04 13:44:22 +0200

    Ejercicio04

---
+++ src/ejercicio04/Electrodomestico.java
@@ -0,0 +1,85 @@
+package ejercicio04;
+
+public class Electrodomestico {
+	enum Energia {
+		A, B, C, D, E, F
+	}
+
+	enum Colores {
+		blanco, negro, rojo, azul, gris
+	}
+
+	protected float precioBase;
+	protected Colores color = Colores.blanco;
+	protected Energia eficienciaEnergetica = Energia.F;
+	protected float peso = 5;
+
+	public Electrodomestico() {
+
+	}
+
+	public Electrodomestico(float precioBase, String color, char eficienciaEnergetica, float peso) {
+		super();
+		if (precioBase > 0) {
+			this.precioBase = precioBase;
+		}
+		comprobarConsumoEnergetico(eficienciaEnergetica);
+		comprobarColor(color);
+		if (peso > 0) {
+			this.peso = peso;
+		}
+	}
+
+	public float getPrecioBase() {
+		return precioBase;
+	}
+
+	public void setPrecioBase(float precioBase) {
+		if (precioBase > 0) {
+			this.precioBase = precioBase;
+		}
+	}
+
+	public Colores getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = Colores.valueOf(color);
+	}
+
+	public Energia getEficienciaEnergetica() {
+		return eficienciaEnergetica;
+	}
+
+	public void setEficienciaEnergetica(String eficienciaEnergetica) {
+		this.eficienciaEnergetica = Energia.valueOf(eficienciaEnergetica);
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		if (peso > 0) {
+			this.peso = peso;
+		}
+	}
+	
+	private void comprobarConsumoEnergetico(char letra) {
+		if (letra == 'A' || letra == 'B' || letra == 'C' || letra == 'D' || letra == 'E' || letra == 'F') {
+			this.eficienciaEnergetica = Energia.valueOf(String.valueOf(letra));
+		} else {
+			this.eficienciaEnergetica = Energia.F;
+		}
+	}
+
+	private void comprobarColor(char letra) {
+		if (letra == 'A' || letra == 'B' || letra == 'C' || letra == 'D' || letra == 'E' || letra == 'F') {
+			this.eficienciaEnergetica = Energia.valueOf(String.valueOf(letra));
+		} else {
+			this.eficienciaEnergetica = Energia.F;
+		}
+	}
+	
+}
